3bb3eeee34fbb1d601f4b928173f1face43df483
alexrsit <alexanderolanderrosta@hotmail.com>
2025-01-14 09:32:31 +0100

    add project

---
+++ pkg/api/api.go
@@ -0,0 +1,113 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func GetAccessToken(region string) (string, error) {
+	clientID, clientSecret := os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET")
+
+	if clientID == "" || clientSecret == "" {
+		return "", fmt.Errorf("missing client ID or client secret")
+	}
+
+	url := fmt.Sprintf("https://%s.battle.net/oauth/token", region)
+	req, err := http.NewRequest("POST", url, strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		return "", err
+	}
+	req.SetBasicAuth(clientID, clientSecret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "", err
+	}
+
+	accessToken, ok := result["access_token"].(string)
+	if !ok {
+		return "", fmt.Errorf("unable to get access token")
+	}
+	return accessToken, nil
+}
+
+func GetCharacterProfile(accessToken, region, realm, character string) (map[string]interface{}, error) {
+	endpoints := []string{
+		fmt.Sprintf("https://%s.api.blizzard.com/profile/wow/character/%s/%s?namespace=profile-%s&locale=en_US&access_token=%s", region, realm, character, region, accessToken),
+		fmt.Sprintf("https://%s.api.blizzard.com/profile/wow/character/%s/%s/collections/mounts?namespace=profile-%s&locale=en_US&access_token=%s", region, realm, character, region, accessToken),
+		fmt.Sprintf("https://%s.api.blizzard.com/profile/wow/character/%s/%s/titles?namespace=profile-%s&locale=en_US&access_token=%s", region, realm, character, region, accessToken),
+		fmt.Sprintf("https://%s.api.blizzard.com/profile/wow/character/%s/%s/character-media?namespace=profile-%s&locale=en_US&access_token=%s", region, realm, character, region, accessToken),
+		fmt.Sprintf("https://%s.api.blizzard.com/profile/wow/character/%s/%s/statistics?namespace=profile-%s&locale=en_US&access_token=%s", region, realm, character, region, accessToken),
+	}
+
+	type apiResponse struct {
+		data map[string]interface{}
+		err  error
+	}
+
+	ch := make(chan apiResponse, len(endpoints))
+
+	for _, url := range endpoints {
+		go func(url string) {
+			data, err := FetchAPI(url, accessToken)
+			ch <- apiResponse{data, err}
+		}(url)
+	}
+
+	combinedData := make(map[string]interface{})
+	for range endpoints {
+		response := <-ch
+		if response.err != nil {
+			return nil, response.err
+		}
+		for k, v := range response.data {
+			combinedData[k] = v
+		}
+	}
+
+	return combinedData, nil
+}
+
+func FetchAPI(url, accessToken string) (map[string]interface{}, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
---
+++ pkg/models/models.go
@@ -0,0 +1,90 @@
+package models
+
+type CharacterProfile struct {
+	Level             int    `json:"level"`
+	Name              string `json:"name"`
+	ItemLevel         int    `json:"average_item_level"`
+	AchievementPoints int    `json:"achievement_points"`
+	Faction           struct {
+		Name string `json:"name"`
+	} `json:"faction"`
+	Guild struct {
+		Name string `json:"name"`
+	} `json:"guild"`
+	Class struct {
+		Name string `json:"name"`
+	} `json:"character_class"`
+	ActiveSpec struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"active_spec"`
+	Health    int `json:"health"`
+	PowerType struct {
+		Name string `json:"name"`
+	} `json:"power_type"`
+	Power   int `json:"power"`
+	Stamina struct {
+		Effective int `json:"effective"`
+	} `json:"stamina"`
+}
+
+type TitlesOwned struct {
+	Titles []struct {
+		Name string `json:"name"`
+	} `json:"titles"`
+}
+
+type MountsOwned struct {
+	Mounts []struct {
+		Mount struct {
+			Name string `json:"name"`
+		} `json:"mount"`
+	} `json:"mounts"`
+}
+
+type CharacterMedia struct {
+	Assets []struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
+	} `json:"assets"`
+}
+
+type CharacterData struct {
+	Name              string
+	Level             int
+	ItemLevel         int
+	AchievementPoints int
+	Faction           struct {
+		Name string
+	}
+	ActiveSpec struct {
+		Name string
+	}
+	Class struct {
+		Name string
+	}
+	Titles          TitlesOwned
+	Mounts          MountsOwned
+	CharacterImages CharacterMedia
+	MainRawImage    string
+	Guild           struct {
+		Name string
+	}
+	Health    int
+	PowerType struct {
+		Name string
+	}
+	Power   int
+	Stamina struct {
+		Effective int
+	}
+}
+
+func (cm *CharacterMedia) GetMainRawImage() string {
+	for _, asset := range cm.Assets {
+		if asset.Key == "main-raw" {
+			return asset.Value
+		}
+	}
+	return ""
+}
